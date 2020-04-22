1a2b4b97d6cce4af2dbd5552b20c59a93b394dcf
daniel makarzec <danielmakarzec@gmail.com>
2020-04-22 19:25:08 +0100

    create avatar attachment. Add default avatar to assets. edit devise views adding new filds in the forms for username and avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: { case_sensitive: false }
   validates :username, length: { in: 3...16 }
+
+  has_one_attached :avatar
+
+  after_commit :add_default_avatar, on: [:create, :update]
+
+  private
+
+  def add_default_avatar
+    avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'avatar_default.png')), filename: 'avatar_default.png', content_type: 'image/png') unless avatar.attached?
+  end
 end
