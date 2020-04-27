ba35b793b4aec267537fa80de7d80a1b21d88454
daniel makarzec <danielmakarzec@gmail.com>
2020-04-27 18:54:02 +0100

    add custom fonts and components style (footer and pagination

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 global.$ = jQuery;
+require("bootstrap/dist/js/bootstrap")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,7 +18,7 @@ global.$ = jQuery;
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import './bootstrap_js.js';
+import './bootstrap_js';
 import '@fortawesome/fontawesome-free/js/all';
 
 require("trix")
